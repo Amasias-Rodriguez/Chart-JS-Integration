835c8b500b2f37a6b314497ee5609331688af993
Rod-aDev <100006082+Rod-aDev@users.noreply.github.com>
2022-08-12 22:49:22 -0600

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,7 +1,18 @@
+// Menu toggle
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function(){
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+}
+
+
 let list = document.querySelectorAll('.navigation li');
 function activeLink(){
     list.forEach((item) =>
-    item.classList.remove(''));
+    item.classList.remove('hovered'));
     this.classList.add('hovered');
 }
 
